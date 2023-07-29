fa3d4087c9802f012d917110ab5585120d145662
macjonessoriano <sorianomacjones@gmail.com>
2023-07-29 21:28:29 +0800

    initial commit by the vid-94 ExpressRouters

---
+++ models/friends.model.js
@@ -0,0 +1,20 @@
+const friends = [
+	{
+		id: 0,
+		name: 'Emmanuel',
+	},
+	{
+		id: 1,
+		name: 'Princess',
+	},
+	{
+		id: 2,
+		name: 'Glydell',
+	},
+	{
+		id: 3,
+		name: 'Rod Abraham',
+	},
+];
+
+module.exports = friends;
