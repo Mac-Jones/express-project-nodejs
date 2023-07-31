b477c3b99eb7a1f2812b92a72b5624c242f4f79d
macjonessoriano <sorianomacjones@gmail.com>
2023-07-31 22:28:30 +0800

    first express.js API

--- controllers/messages.controller.js
+++ controllers/messages.controller.js
@@ -1,5 +1,11 @@
+const path = require('path');
+
 function getMessages(req, res) {
-	res.send('<ul><li>Hello po sainyo</li></ul>');
+	// res.sendFile(path.join(__dirname, '..', 'public', 'images', 'wife.jpg'));
+	res.render('messages', {
+		title: 'my friends',
+		friend: 'Rod Abraham Melano',
+	});
 }
 
 function postMessage(req, res) {
